e93840ba533bcc7f12cdc446a79e39e1373c5ae1
freakyAion <FreakyAion@yandex.ru>
2023-05-22 01:23:48 +0500

    WORKS

--- script.js
+++ script.js
@@ -11,24 +11,45 @@ launch.addEventListener("click", runSim);
 
 function runSim()
 {
-    for(let i = 0; i<=300; i++)
-    {
-        setTimeout(() => {
-        }, 10 * i);
-    }
-}
-
-function positionCalc(){
     let lef_ = projectile.style.left;
         lef_ = lef_.substring(0, lef_.length - 2);
+        lef_ -= 0;
     let bot_ = projectile.style.bottom;
         bot_ = bot_.substring(0, bot_.length - 2);
-    bot_ = bot_ + 1 - 1;
-    let projectileVelocity = velocity.value;
-    projectileVelocity = projectileVelocity + 1 - 1;
-    bot_ = bot_ + projectileVelocity;
-    console.log(bot_);
-    projectile.style.bottom = bot_ + "px";
-    console.log(projectile.style.bottom);
+        bot_ -= 0;
 
+    let projectileVelocity = velocity.value;
+    let projectileAngle = angle.value;
+        projectileVelocity -= 0
+        projectileAngle -= 0;
+
+    let velocityX = Math.cos(projectileAngle) * projectileVelocity;
+    let velocityY = Math.sin(projectileAngle) * projectileVelocity;
+    let velocityG = gravity.value;
+    velocityG -= 0;
+
+    positionCalc(velocityY, velocityX, velocityG, bot_, lef_);
+}
+
+function positionCalc(velocityY, velocityX, velocityG, bot_, lef_){
+    for(let i = 1; i<=300; i++)
+    {
+        setTimeout(()=>
+        {
+            velocityY -= velocityG;
+
+            bot_ = bot_ + velocityY;
+            lef_ = lef_ + velocityX;
+        
+        
+            projectile.style.bottom = bot_ + "px";
+            projectile.style.left = lef_ + "px";
+        
+            console.log("Done, Iteration №" + i);
+            console.log("Debug Data");
+            console.log("");
+            console.log("Velocity on the X axis is equal to " + velocityX);
+            console.log("Velocity on the Y axis is equal to " + velocityY);
+        }, 50 * i);
+    }
 }
