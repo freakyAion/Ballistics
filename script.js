147180210dc71814b0fedd517ddf64aa9a0b4909
freakyAion <FreakyAion@yandex.ru>
2023-05-25 01:41:08 +0500

    Acceleration calculation is wrong

--- script.js
+++ script.js
@@ -3,8 +3,17 @@ const angle = document.querySelector("#angle");
 const gravity = document.querySelector("#gravity");
 const density = document.querySelector("#density");
 const mass = document.querySelector("#mass");
-const surfaceArea = Math.PI * 35/2 * 35/2;
+const surfaceArea = (Math.PI * 35/2 * 35/2) / 100;
 const Cf = 0.47;
+
+let environmentalDensity = density.value;
+let projectileMass = mass.value
+
+const borderX = document.querySelector(".sim").offsetWidth;
+const borderY = document.querySelector(".sim").offsetHeight;
+
+let debugHorisontal = document.querySelector(".horisontal").style.translate;
+let debugVertical = document.querySelector(".vertical").style.translate;
 
 const projectile = document.querySelector(".projectile");
 
@@ -21,36 +30,48 @@ function runSim()
         bot_ = bot_.substring(0, bot_.length - 2);
         bot_ -= 0;
 
-    let projectileVelocity = force.value / mass.value;
+    let projectileAcceleration = force.value / mass.value;
     let projectileAngle = angle.value;
-        projectileVelocity -= 0
+        projectileAcceleration -= 0
         projectileAngle -= 0;
 
-    let velocityX = Math.cos(projectileAngle) * projectileVelocity;
-    let velocityY = Math.sin(projectileAngle) * projectileVelocity;
-    let velocityG = gravity.value;
-    velocityG -= 0;
+    let accelerationX = Math.cos((projectileAngle * Math.PI) / 180) * projectileAcceleration;
+    let accelerationY = Math.sin((projectileAngle * Math.PI) / 180) * projectileAcceleration;
+    let accelerationG = gravity.value;
+    accelerationG -= 0;
 
-    positionCalc(velocityY, velocityX, velocityG, bot_, lef_);
+    let velocityX = 0;
+    let velocityY = 0;
+
+    environmentalDensity -= 0;
+    projectileMass -= 0;
+
+    positionCalc(velocityX, velocityY, accelerationX, accelerationY, accelerationG, bot_, lef_, environmentalDensity, projectileMass);
 }
 
-function positionCalc(velocityY, velocityX, velocityG, bot_, lef_){
-    for(let i = 1; i<=900; i++)
+function positionCalc(velocityY, velocityX, accelerationX, accelerationY, accelerationG, bot_, lef_, environmentalDensity, projectileMass){
+    for(let i = 1; i<=100; i++)
     {
         setTimeout(()=>
         {
-            let resistanceY = (Cf * ((density.value * velocityY) / 2 )* surfaceArea) / mass.value;
-            let resistanceX = (Cf * ((density.value * velocityX) / 2 )* surfaceArea) / mass.value;
+            let resistanceY = (Cf * ((environmentalDensity * velocityY) / 2 )* surfaceArea) / projectileMass;
+            let resistanceX = (Cf * ((environmentalDensity * velocityX) / 2 )* surfaceArea) / projectileMass;
 
-            resistanceX *= 0.001;
-            resistanceY *= 0.001;
+            resistanceX *= 0.01;
+            resistanceY *= 0.01;
 
-            velocityY -= velocityG;
+            accelerationY -= accelerationG;
 
-            bot_ = bot_ + velocityY - resistanceY;
-            lef_ = lef_ + velocityX - resistanceX;
+            accelerationX -= resistanceX;
+            accelerationY -= resistanceY;
+
+            velocityX += accelerationX;
+            velocityY += accelerationY;
         
         
+            lef_ += velocityX;
+            bot_ += velocityY;
+
             projectile.style.bottom = bot_ + "px";
             projectile.style.left = lef_ + "px";
         
@@ -59,8 +80,15 @@ function positionCalc(velocityY, velocityX, velocityG, bot_, lef_){
             console.log("");
             console.log("Velocity on the X axis is equal to " + velocityX);
             console.log("Velocity on the Y axis is equal to " + velocityY);
+            console.log("Acceleration on the X axis is equal to " + accelerationX);
+            console.log("Acceleration on the Y axis is equal to " + accelerationY);
             console.log("Resitance on the X axis is equal to " + resistanceX);
             console.log("Resistance on the Y axis is equal to " + resistanceY);
-        }, 50 * i);
+
+            console.log("");
+            console.log("");
+            console.log("");
+
+        }, 2000 * i);
     }
 }
